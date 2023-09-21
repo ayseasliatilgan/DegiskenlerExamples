a196428f956c2ec0620ad05ed5b94d95771f8ac8
aasli <aasli@ASLINB>
2023-09-21 14:34:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        // ogrenci giris sistemi icin ornek
+        var studentName = ("Susan");
+        var studentSurname = ("Olsen");
+        var nameSurname = studentName + studentSurname;
+        var studentNo = "1013";
+        var studentBirthDate = 1997;
+        var studentAge = 2023 - studentBirthDate;
+        var studentAdress = "London UK";
+
+        Console.WriteLine(studentName);
+        Console.WriteLine(studentSurname);
+        Console.WriteLine(nameSurname);
+        Console.WriteLine(studentNo);
+        Console.WriteLine(studentBirthDate);
+        Console.WriteLine(studentAge);
+        Console.WriteLine(studentAdress);
+
+
+    }
+}
